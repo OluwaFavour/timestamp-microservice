9c1fa106c0d8237718ba5c090964749611c46080
Favour Olusayo <fsticks8187@gmail.com>
2024-04-21 04:05:51 +0000

    Add timestamp api endpoints

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +21,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
@@ -30,3 +30,25 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// API handler
+function APIhandler(req, res) {
+  const string = req.params.date;
+  let message;
+
+  // Check if string is not empty
+  if (string) {
+    // Check if string contains numbers only and parse to integer before creating date object if true
+    const date = /^\d+$/.test(string) ? new Date(parseInt(string)) : new Date(string);
+    // Check if date object is a valid date and create message object
+    message = isNaN(date.getTime()) ? { "error": "Invalid Date" } : { "unix": date.getTime(), "utc": date.toUTCString() };
+  }
+  // Create message object with current time if string is empty
+  else {
+    const currentDate = new Date();
+    message = { "unix": currentDate.getTime(), "utc": currentDate.toUTCString() };
+  }
+  res.json(message);
+}
+
+app.get("/api/:date?", APIhandler);
